21bb44a947224ddbadacd2fe9efad9e76cedf22c
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-19 03:37:19 +0630

    add validation in add/update subscription apis

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -5,17 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\Plan;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class SubscriptionController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'plan' => ['required', Rule::exists('plans', 'slug')],
+            'token' => 'required',
+        ]);
+
+        if ($request->plan === 'free') {
+            throw new Exception("you can't store free plan as subscription.");
+        }
+
         $plan = Plan::whereSlug($request->plan)->first();
         auth()->user()->newSubscription('default', $plan->stripe_id)->create($request->paymentMethodId);
     }
 
     public function update(Request $request)
     {
+        $request->validate([
+            'plan' => ['required', Rule::exists('plans', 'slug')],
+        ]);
+
         $plan = Plan::whereSlug($request->plan)->first();
 
         if (!$request->user()->canSwap($plan)) {
